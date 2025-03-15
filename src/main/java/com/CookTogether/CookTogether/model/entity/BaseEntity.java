4e2ed22d6e421a3667d8523ffe74c977066abf13
Toni Iliev <leesin9701@gmail.com>
2025-03-15 17:59:38 +0200

    Create BaseEntity.java

---
+++ src/main/java/com/CookTogether/CookTogether/model/entity/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.CookTogether.CookTogether.model.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+}
